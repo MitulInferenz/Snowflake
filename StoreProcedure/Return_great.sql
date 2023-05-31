cae36d9baf5079778316db41e212649eac4a6d21
MitulInferenz <mitul.inferenz@inferenz.ai>
2023-05-31 15:17:20 +0530

    testfilecreated

---
+++ StoreProcedure/Return_great.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE PROCEDURE return_greater(number_1 INTEGER, number_2 INTEGER)
+RETURNS INTEGER NOT NULL
+LANGUAGE SQL
+AS
+$$
+BEGIN
+  IF (number_1 > number_2) THEN
+    RETURN number_1;
+  ELSE
+    RETURN number_2;
+  END IF;
+END;
+$$
+;
